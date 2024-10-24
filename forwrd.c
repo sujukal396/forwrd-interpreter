5a86bf5ea7a693b33f5525d0b93051141385e027
Sujay Jukalkar <sujukalkar@gmail.com>
2024-10-24 00:44:26 -0400

    Fixed indentation in forwrd.c frfr this time

--- forwrd.c
+++ forwrd.c
@@ -11,16 +11,16 @@ Token* pop(Stack *stack, Token *token) {}
 Token* peek(Stack *stack, int index) {}
 
 int main() {
-  char filepath[] = "wrd_files/factorial.wrd";
-  FILE *fptr = fopen(filepath, "r");
-  char line[100];
-  // handle strtok stuff
-  while(word != NULL) {
-    Token *token = parse_token(word);
-    if(token->type == WRD) {
-      execute_wrd(token);
-    } else {
-      push(stack, token);
+    char filepath[] = "wrd_files/factorial.wrd";
+    FILE *fptr = fopen(filepath, "r");
+    char line[100];
+    // handle strtok stuff
+    while(word != NULL) {
+        Token *token = parse_token(word);
+        if(token->type == WRD) {
+            execute_wrd(token);
+        } else {
+            push(stack, token);
+        }
     }
-  }
 }
