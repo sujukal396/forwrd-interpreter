60ef49395c2e4bfe14449b766e7aee0e4b3368de
Sujay Jukalkar <sujukalkar@gmail.com>
2024-11-01 21:22:42 -0400

    fixed wrd function prototypes

--- wrd.h
+++ wrd.h
@@ -1,36 +1,38 @@
 #ifndef WRD_H
 #define WRD_H
 
-void add();
-void mul();
-void mod();
-void div();
-void equ();
-void lss();
-void gtr();
-void lte();
-void gte();
-void and();
-void ior();
-void not();
-void bnd();
-void bor();
-void bnt();
-void bxr();
-void lsh();
-void rsh();
-void swp();
-void def();
-void sel();
-void get();
-void fch();
-void prt();
-void sld();
-void eva();
-void lst();
-void len();
-void pop();
-void dup();
-void clr();
+#include "vector.h"
+
+void add(Deque *stack);
+void mul(Deque *stack);
+void mod(Deque *stack);
+void div(Deque *stack);
+void equ(Deque *stack);
+void lss(Deque *stack);
+void gtr(Deque *stack);
+void lte(Deque *stack);
+void gte(Deque *stack);
+void and(Deque *stack);
+void ior(Deque *stack);
+void not(Deque *stack);
+void bnd(Deque *stack);
+void bor(Deque *stack);
+void bnt(Deque *stack);
+void bxr(Deque *stack);
+void lsh(Deque *stack);
+void rsh(Deque *stack);
+void swp(Deque *stack);
+void def(Deque *stack);
+void sel(Deque *stack);
+void get(Deque *stack);
+void fch(Deque *stack);
+void prt(Deque *stack);
+void sld(Deque *stack);
+void eva(Deque *stack);
+void lst(Deque *stack);
+void len(Deque *stack);
+void pop(Deque *stack);
+void dup(Deque *stack);
+void clr(Deque *stack);
 
 #endif // !WRD_H
